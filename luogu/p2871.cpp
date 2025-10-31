8a284d3888d8ffacaa110cb750c9bc961e9b4fb6
aurxs <1562508557@qq.com>
2025-10-31 20:59:13 +0800

    p2871

---
+++ luogu/p2871.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    int l, n;
+    cin >> n >> l;
+    vector<pair<int, int>> m(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> m[i].first >> m[i].second;
+    }
+    vector dp(l + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        int w = m[i].first;
+        int v = m[i].second;
+        for (int j = l; j >= w; j--) {
+            dp[j] = max(dp[j], dp[j - w] + v);
+        }
+    }
+    cout << dp[l] << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // int T;
+    // cin >> T;
+    // while (T--) {
+    //     solve();
+    // }
+    solve();
+
+    return 0;
+}

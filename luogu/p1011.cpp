212e6975191c3bcf9eed7e1e2c0e34944d0e5604
Aurxs <1562508557@qq.com>
2025-11-04 19:12:19 +0800

    p1011

--- luogu/p1011.cpp
+++ luogu/p1011.cpp
@@ -19,15 +19,19 @@ int main() {
     }
 
     // Ba[i], Bk[i]: 第 i 站上车人数中 a 和 k 的系数
-    // Sa[i], Sk[i]: 第 i 站开出时车上人数中 a 和 k 的系数
+    // Sa[i], Sk[i]: 第 i 站开出时车上人数中 a 和 k 的系数˚
     vector<long long> Ba(n), Bk(n), Sa(n), Sk(n);
 
     // 初始化第 1, 2 站
-    Ba[0] = 1; Bk[0] = 0; // B[1] = 1*a + 0*k
-    Ba[1] = 0; Bk[1] = 1; // B[2] = 0*a + 1*k
+    Ba[0] = 1;
+    Bk[0] = 0; // B[1] = 1*a + 0*k
+    Ba[1] = 0;
+    Bk[1] = 1; // B[2] = 0*a + 1*k
 
-    Sa[0] = 1; Sk[0] = 0; // S[1] = 1*a + 0*k
-    Sa[1] = 1; Sk[1] = 0; // S[2] = 1*a + 0*k
+    Sa[0] = 1;
+    Sk[0] = 0; // S[1] = 1*a + 0*k
+    Sa[1] = 1;
+    Sk[1] = 0; // S[2] = 1*a + 0*k
 
     // 从第 3 站开始递推到第 n-1 站
     for (int i = 2; i < n - 1; ++i) {
@@ -45,7 +49,8 @@ int main() {
 
     if (x <= 2) {
         cout << a << endl;
-    } else {
+    }
+    else {
         // S[x] = Sa[x-1]*a + Sk[x-1]*k
         cout << Sa[x - 1] * a + Sk[x - 1] * k << endl;
     }
